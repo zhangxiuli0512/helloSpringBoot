1e9c36d02b684db47fceedbf19ddf28362069f07
zhangxiuli0512 <593243283.qq.com>
2020-06-05 16:49:35 +0800

    add class

---
+++ src/main/java/com/example/springboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	                                 //Application的类名
+	     return application.sources(HelloSpringBootApplication.class);
+	 }
+
+}
